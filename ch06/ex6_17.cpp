a66b3dc0dbe2700e62e298123e3794d946204830
oyx <657531018@qq.com>
2019-01-16 17:15:13 +0800

    commit

--- ch06/ex6_17.cpp
+++ ch06/ex6_17.cpp
@@ -12,6 +12,7 @@ bool hasUppercase(const string& str)
 
 const string& makeLowercase(string& str)
 {
+    //如果要修改变量则需要加引用
     for (auto& c : str)
         if (isupper(c)) c = tolower(c);
     return str;
@@ -19,7 +20,14 @@ const string& makeLowercase(string& str)
 
 int main()
 {
+    /*
+    Sets the boolalpha format flag for the str stream.
+    When the boolalpha format flag is set, bool values are
+    inserted/extracted by their textual representation:
+    either true or false, instead of integral values.
+    */
     string str("Hello World!");
     std::cout << std::boolalpha << hasUppercase(str) << std::endl;
     std::cout << makeLowercase(str) << std::endl;
+    system("pause");
 }
